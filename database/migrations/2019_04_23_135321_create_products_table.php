bbe85326382d8556ec3e411c89861820a53b8bd7
mcjovial <emmanuelelias455@gmail.com>
2021-10-21 17:06:48 +0100

    initial commit

---
+++ database/migrations/2019_04_23_135321_create_products_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('supplier_id')->nullable();
+            $table->string('stock');
+            $table->string('image')->nullable();
+            $table->dateTime('buying_date');
+            $table->dateTime('expire_date')->nullable();
+            $table->float('cost_price_pack');
+            $table->float('cost_price_bottle');
+            $table->float('sell_price_bottle');
+            $table->float('launch_price');
+            $table->integer('bottles_per_pack');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('supplier_id')->references('id')->on('suppliers')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
