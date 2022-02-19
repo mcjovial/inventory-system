e445e00c7ddb2bf0a9c2cc1d551e3ca313bf7232
mcjovial <emmanuelelias455@gmail.com>
2022-02-19 18:54:34 +0100

    table responsive

--- resources/views/admin/balance/index.blade.php
+++ resources/views/admin/balance/index.blade.php
@@ -38,7 +38,7 @@
                             </div>
                             <!-- /.card-header -->
                             <div class="card-body">
-                                <table id="example1" class="table table-bordered table-striped text-center table-responsive">
+                                <table id="example1" class="table table-bordered table-striped text-center table-responsive-lg">
                                     <thead>
                                     <tr>
                                         <th>Serial</th>
